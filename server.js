8737573df7ef5296baf51d0e777c909696366dd1
qwl8gh <qwl8gh@gmail.com>
2024-05-04 14:57:14 -0700

    1714859827

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const { checkUserPrivilege } = require('./build/Release/checkPrivilegeAddon.node');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -28,7 +29,10 @@ app.post('/check', (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
     console.log(`Server is running on http://localhost:${PORT}`);
+    import('open')
+        .then(open => open.default(`http://localhost:${PORT}`))
+        .then(() => console.log('Browser opened'))
+        .catch(err => console.error('Error opening browser:', err));
 });
